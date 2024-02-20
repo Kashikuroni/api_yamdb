24084707ef00c04a1f36cf7b147fe6d3a8a8faa4
Никита Тарасов <owlramps@yandex.ru>
2024-02-20 14:48:12 +0300

    Register apps, and first migrations

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+  class Meta:
+    db_tbale = 'reviews_review'
+    verbose_name = 'Отзыв'
+    verbose_name_plural = 'Отзывы'
+
+  def __str__(self) -> str:
+    return super().__str__()
+
+
+class Comment(models.Model):
+  class Meta:
+    db_tbale = 'reviews_comment'
+    verbose_name = 'Комментарий'
+    verbose_name_plural = 'Комментарии'
+
+  def __str__(self) -> str:
+    return super().__str__()
+
+
+class Rating(models.Model):
+  class Meta:
+    db_tbale = 'reviews_rating'
+    verbose_name = 'Рейтинг'
+    verbose_name_plural = 'Рейтинги'
+
+  def __str__(self) -> str:
+    return super().__str__()
