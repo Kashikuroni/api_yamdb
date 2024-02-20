faa5129e73d3189aa6b071dc2c907fc94b55ee99
Сергей Орлов <Arel170683@yandex.ru>
2024-02-20 23:17:30 +0300

    update user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
 
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=254)
+    username = models.CharField(max_length=150, unique=True)
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
 
     def __str__(self):
         return self.username
+
+    def clean(self):
+        super().clean()
+        if self.username == 'me':
+            raise ValidationError(
+                'Использовать имя "me" в качестве username запрещено.'
+            )
